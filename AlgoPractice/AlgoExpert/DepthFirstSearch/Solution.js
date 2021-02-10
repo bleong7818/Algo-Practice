39f206add5938a12d02987e4b830a1bf2e1c8324
Brandon Leong <bjtl1105@gmail.com>
2021-02-10 15:49:59 -0800

    debugging

---
+++ AlgoPractice/AlgoExpert/DepthFirstSearch/Solution.js
@@ -0,0 +1,26 @@
+//time = O(v + e) where v = vertices and e = edges (branches)
+//space = O(v) length of returned array has a length of v.
+
+class Node {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
+
+    addChild(name) {
+        this.children.push(new Node(name));
+        return this;
+    }
+
+    depthFirstSearch(array) {
+        // Write your code here.
+        array.push(this.name)
+        for (const child of this.children) {
+            child.depthFirstSearch(array);
+        }
+        return array;
+    }
+}
+
+// Do not edit the line below.
+exports.Node = Node;
---
+++ AlgoPractice/AlgoExpert/MinWaitingTime/mySolution.js
@@ -0,0 +1,18 @@
+//Min waiting time given a non empty array of integers
+//{"queries": [3, 2, 1, 2, 6]} 
+// sorted = [1, 2, 2, 3, 6]
+//output = 17 = (0 + 1 + 3 + 5 + 8)
+
+
+
+function minimumWaitingTime(queries) {
+    // Write your code here.
+    queries.sort((a, b) => a - b);
+    let totalWaitingTime = queries[0];
+    for (let i = 1; i < queries.length - 1; i++) {
+        const currentNum = queries[i];
+        let newWaitingTime = currentNum + totalWaitingTime;
+        totalWaitingTime += newWaitingTime;
+    }
+    return totalWaitingTime;
+}

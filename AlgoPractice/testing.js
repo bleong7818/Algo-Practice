08aa22860577feeb6f9ef69525e48fa0d534f3fc
Brandon Leong <bjtl1105@gmail.com>
2021-04-06 17:39:20 -0700

    did last problem for the day

--- AlgoPractice/testing.js
+++ AlgoPractice/testing.js
@@ -1,6 +1,19 @@
-function mango(quantity, price) {
-    return price * (quantity - Math.floor(quanitity / 3));
+var maxSequence = function (arr) {
+    if (arr.length === 0) return 0;
+    let currentMax = 0;
+    let bigMax = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        currentMax = currentMax + arr[i];
+        if (bigMax < currentMax) {
+            bigMax = currentMax;
+        }
+        if (currentMax < 0) {
+            currentMax = 0;
+        }
+    }
+    return bigMax;
 }
 
-console.log(mango(3, 3));
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 // console.log()

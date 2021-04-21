344658232f390fe01a4e91e8bbce06d38cbde088
Brandon Leong <bjtl1105@gmail.com>
2021-04-21 12:24:15 -0700

    did more leetcode problems

---
+++ AlgoPractice/LeetCode_Problems/Apr19-Apr23/cookies.js
@@ -0,0 +1,15 @@
+var findContentChildren = function (g, s) {
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+    let j = 0;
+    let res = 0;
+
+    for (let num of s) {
+        if (num >= g[j]) {
+            res++;
+            j++;
+        }
+
+    }
+    return res;
+};

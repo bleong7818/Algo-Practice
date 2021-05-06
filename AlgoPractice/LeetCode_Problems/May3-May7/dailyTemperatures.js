0ed7c4f380d6a89ac11d487f5ae40aea45adb920
Brandon Leong <bjtl1105@gmail.com>
2021-05-05 17:32:22 -0700

    finished leetcode problems

--- AlgoPractice/LeetCode_Problems/May3-May7/dailyTemperatures.js
+++ AlgoPractice/LeetCode_Problems/May3-May7/dailyTemperatures.js
@@ -6,7 +6,6 @@ var dailyTemperatures = function (T) {
         let waitTillWarm = 0;
         for (let j = i + 1; j < T.length; j++) {
             let jTemp = T[j];
-
             if (temp < jTemp) {
                 waitTillWarm = j - i;
                 break;
@@ -16,7 +15,3 @@ var dailyTemperatures = function (T) {
     }
     return daysWait;
 };
-
-let T = [73, 74, 75, 71, 69, 72, 76, 73];
-
-console.log(dailyTemperatures(T));
---
+++ AlgoPractice/LeetCode_Problems/May3-May7/nextGreatestLetter.js
@@ -0,0 +1,3 @@
+var nextGreatestLetter = function (letters, target) {
+    return letters.filter(x => x.charCodeAt(0) > target.charCodeAt(0))[0] || letters[0];
+};

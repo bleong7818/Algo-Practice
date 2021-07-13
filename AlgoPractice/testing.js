dbfa1933b1ec683271c45b6635a31cc6bde872d7
Brandon Leong <bjtl1105@gmail.com>
2021-07-13 10:43:35 -0700

    added probss

--- AlgoPractice/testing.js
+++ AlgoPractice/testing.js
@@ -1,6 +1,27 @@
-numbers = [1, 2, 3];
-numbersCopy = [...numbers];
+let string = "0100111";
 
-numbersCopy.sort((a, b) => b - a);
-console..log(nu)
-console.log(numbersCopy);
+function areEqual(s) {
+    let zeroCount = 0;
+    let oneCount = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "1") {
+            oneCount++;
+        } else {
+            zeroCount++;
+        }
+    }
+    return zeroCount === oneCount;
+}
+
+function grouped(s) {
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "1") {
+            if (s[i - 1] !== "1" && s[i + 1] !== "1") return false;
+        } else {
+            if (s[i - 1] !== "0" && s[i + 1] !== "0") return false;
+        }
+    }
+    return true;
+}
+
+console.log(grouped(string));

11f7a9bc50e3dc1350dca8c783b28e5c8acc3858
Brandon Leong <bjtl1105@gmail.com>
2021-08-05 19:13:27 -0700

    hello

--- AlgoPractice/testing.js
+++ AlgoPractice/testing.js
@@ -1,31 +1,7 @@
-function howMany(sentence) {
-    let words = 0;
-    let potentialWords = sentence.split(" ");
-    for (let i = 0; i < potentialWords.length; i++) {
-        if (isWord(potentialWords[i])) words++;
-    }
-
-    return words;
+function isPalindrome(string) {
+    // Write your code here.
+    let test = string.split("").reverse().join("");
+    return string === test;
 }
 
-function isWord(word) {
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz-ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let punctuation = ".,?!";
-    for (let i = 0; i < word.length; i++) {
-        if (i === word.length - 1) {
-            if (!punctuation.includes(word[i]) && !alphabet.includes(word[i])) {
-                return false;
-            }
-        } else {
-            if (!alphabet.includes(word[i])) {
-                return false;
-            }
-        }
-    } 
-
-    return true;
-}
-
-
-let gibberish = "b? Dl )B 4(V! A. MK, YtG ](f 1m )CNxuNUR {PG?";
-console.log(howMany(gibberish));
+console.log(isPalindrome("aab"));

21fc2926bb5163eb5babaeb803ced0afba82d901
Brandon Leong <bjtl1105@gmail.com>
2021-08-12 19:14:42 -0700

    yo

--- AlgoPractice/testing.js
+++ AlgoPractice/testing.js
@@ -1,36 +1,23 @@
-var findJudge = function (n, trust) {
-    //     check 1st position if its N - return false
-    //     check that 2nd position is N for all arrays.
+function rollTheString(s, roll) {
+    let newStringSplit = s.split("");
+    for (let i = 0; i < roll.length; i++) {
+        let rollNum = roll[i];
+        for (let j = 0; j <= rollNum; j++) {
+            let currentChar = newStringSplit[j];
+            let newChar = rollChar(currentChar);
+            newStringSplit[j] = newChar;
 
-    let hash = {};
-
-    for (let i = 1; i <= n; i++) {
-        hash[i] = [[], []];
-    }
-    
-    for (let j = 0; j < trust.length; j++) {
-        let mini = trust[j];
-        let truster = mini[0];
-        let trusted = mini[1];
-        
-        hash[truster][0].push(trusted);
-        hash[trusted][1].push(truster);
-    }
-    
-    // let keyArray = Object.keys(hash);
-    console.log(hash);
-    for (let key in hash) {
-        // debugger
-        if (hash[key][0].length === 0) {
-            // debugger
-            if ((n - 1) == hash[key][1].length) {
-                return key;
-            }
-            //                 let newArray = keyArray.slice(0, index) + keyArray.slice(index + 1);
         }
     }
+    return newStringSplit.join("");
+}
 
-    return -1;
-};
+function rollChar(char) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let charIndex = alphabet.indexOf(char);
+    let newChar = alphabet[(charIndex + 1) % 26];
+    return newChar;
+}
 
-console.log(findJudge(3, [[1, 3], [2, 3]]));
+let string = "abz";
+let roll = [3, 2, 1];

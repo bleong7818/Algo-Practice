d28ec1b691d5474869f7dee16102b04a4dec893c
Brandon Leong <bjtl1105@gmail.com>
2021-09-01 19:50:23 -0700

    code

--- AlgoPractice/testing.js
+++ AlgoPractice/testing.js
@@ -1,34 +1,4 @@
-function maximumContainers(scenarios) {
-    if (scenarios.length !== 3) return false;
-    let money = scenarios[0];
-    let cost = scenarios[1];
-    let emptyExchange = scenarios[2];
-    let remainder = 0;
-    let containers = Math.floor(money / cost);
-    let total = containers;
-    while (containers > 1) {
-        // console.log(containers)
-        remainder += (containers % emptyExchange);
-        containers = Math.floor(containers / emptyExchange);
-        total += containers;
-        containers += remainder;
-        if (containers < cost) break;
-        remainder = 0;
-    }
-    return total;
-}
-
-// 3
-// 10 2 5
-// 12 4 4
-// 6 2 2
-
-// 3 
-console.log(maximumContainers([3]));
-console.log(maximumContainers([10, 2, 5]));
-console.log(maximumContainers([12, 4, 4]));
-console.log(maximumContainers([6, 2, 2]));
-
-// 6
-// 3
-// 5
+let array = [1, 2, 3, 4, 6];
+let leftSlice = array.slice(0, 3);
+let rightSlice = array.slice(4);
+console.log(leftSlice, rightSlice);

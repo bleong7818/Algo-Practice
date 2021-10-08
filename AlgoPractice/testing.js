6257ce508f25594a6dc4a54b38e9b3d7feaeecb2
Brandon Leong <bjtl1105@gmail.com>
2021-10-07 18:34:29 -0700

    yo

---
+++ AlgoPractice/testing.js
@@ -0,0 +1,34 @@
+function itemsSort(items) {
+    // Write your code here
+    let itemHash = {};
+    let newArray = [];
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        if (itemHash[item]) {
+            itemHash[item]++;
+        } else {
+            itemHash[item] = 1;
+        }
+    }
+
+    // for (const key of itemHash) {
+    //     if (itemHash[key] = 1) {
+
+    //     }
+    // }
+    console.log(itemHash);
+    return getSortedHash(itemHash);
+}
+function getSortedHash(inputHash) {
+    var resultHash = {};
+
+    var keys = Object.keys(inputHash);
+    keys.sort(function (a, b) {
+        return inputHash[a] - inputHash[b]
+    }).forEach(function (k) {
+        resultHash[k] = inputHash[k];
+    });
+    return resultHash;
+}
+let items = [3, 5, 4, 5, 4, 6];
+console.log(itemsSort(items));

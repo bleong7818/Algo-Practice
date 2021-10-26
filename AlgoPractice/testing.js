d9d65dea008dc829fffb72851f67f04668f008bf
Brandon Leong <bjtl1105@gmail.com>
2021-10-26 12:40:04 -0700

    problems

--- AlgoPractice/testing.js
+++ AlgoPractice/testing.js
@@ -1,23 +1,28 @@
-function getUniqueCharacter(s) {
-    // Write your code here
-    let uniqueHash = {};
-    for (let i = 0; i < s.length; i++) {
-        let char = s[i];
-        if (char in uniqueHash) {
-            uniqueHash[char] = -1;
-        } else {
-            uniqueHash[char] = i + 1;
-        }
-        // debugger
-    }
-    let values = Object.values(uniqueHash).sort((a, b) => a - b);
-    debugger
-    for (let j = 0; j < values.length; j++) {
-        if (values[j] !== -1) {
-            return values[j];
+function minTime(processorTime, taskTime) {
+    
+    let sorted = taskTime.sort((a, b) => a - b);
+    let splitVersion = splitByFour(sorted);
+    let orderedProcess = processorTime.sort((b, a) => a - b);
+    let max = Infinity;
+    for (let i = 0; i < orderedProcess.length; i++) {
+        let processor = orderedProcess[i];
+        let maxTask = splitVersion[i][3];
+        let maxSum = processor + maxTask;
+        debugger
+        if (max > maxSum) {
+            max = maxSum;
         }
     }
-    return -1;
+    return max;
 }
-const stats = "hackthegame";
-console.log(getUniqueCharacter(stats));
+function splitByFour(array) {
+    let splitArray = [];
+    for (let i = 4; i <= array.length; i+= 4) {
+        let piece = array.slice(i - 4, i);
+        splitArray.push(piece);
+    }
+    return splitArray;
+}
+const processorTime = [8, 10];
+const taskTimes = [2, 3, 1, 2, 5, 8, 4, 3];
+console.log(minTime(processorTime, taskTimes));

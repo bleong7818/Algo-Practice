5b631626c7308b396e4db7fcf660698dd9a5ab32
Brandon Leong <bjtl1105@gmail.com>
2021-11-04 19:44:35 -0700

    hopium

--- AlgoPractice/testing.js
+++ AlgoPractice/testing.js
@@ -1,29 +1,8 @@
-function countHighlyProfitableMonths(stockPrices, k) {
-    let profitable = 0;
-    for (let i = 0; i < stockPrices.length - k + 1; i++) {
-        let kMonths = stockPrices.slice(i, i + k);
-        if (isIncreasing(kMonths)) {
-            profitable++;
-        }
-    }
-    return profitable;
-}
+const address = {
+    street: 'Elm Street',
+    city: 'Springfield',
+};
 
-function isIncreasing(months) {
-    for (let i = 0; i < months.length - 1; i++) {
-        let current = months[i];
-        let next = months[i + 1];
-        if (current >= next) {
-            return false;
-        }
-    }
-    return true;
-}
-let prices = [1, 2, 3, 3, 4, 5];
-// let copy = [...prices];
-// let sortedPrices = copy.sort();
-// let otherCopy = [3, 5, 5, 7, 8];
-let k = 3;
-console.log(countHighlyProfitableMonths(prices, k));
-// console.log(prices == sortedPrices);
-// console.log(otherCopy, sortedPrices);
+const {street: s, city: c} = address;
+console.log(s);
+console.log(c);

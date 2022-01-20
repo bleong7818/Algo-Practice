66ebea7f1ac142bff54eae62f34c845bb0ce1006
Brandon Leong <bjtl1105@gmail.com>
2022-01-19 23:57:11 -0800

    html practice

--- AlgoPractice/testing.js
+++ AlgoPractice/testing.js
@@ -1,31 +1,8 @@
-function getMinLength(a, k) {
-    // Write your code here
-    let length = a.length;
+//5 
+// x = 3
+// y = 6
 
-    let maxxedOut = false;
+// 5 != x || != 6
+// 5 != x && != y
 
-    for (let i = 0; i < length; i++) {
-        // maxxedOut = true;
-        let current = a[i];
-        let next = a[i + 1];
-        // let behind = a[i -1];
-        let product = current * next;
-        // debugger
-        if (product <= k) {
-            a[i] = product;
-            a.splice(i + 1, 1);
-            i--;
-            debugger
-        }
-        // debugger
-    }
-    // debugger
-    console.log(a);
-    return a.length;
-}
-
-//sigh
-
-let array = [2, 3, 3, 7, 3, 5];
-let k = 20;
-console.log(getMinLength(array, k));
+if x < 5 && x > 3
